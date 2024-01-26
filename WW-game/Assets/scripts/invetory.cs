d533e63fa6740108c222774d3e80ae4a591e98ce
YodaOnCrack <mykyta006@gmail.com>
2024-01-26 17:29:12 +0100

    inventory system

---
+++ WW-game/Assets/scripts/invetory.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class invetory : MonoBehaviour
+
+{
+
+    ArrayList inventory;
+
+
+    void Start()
+    {
+        inventory = new ArrayList();
+        inventory.Add();
+    }
+
+    void Update()
+    {
+        
+    }
+}
