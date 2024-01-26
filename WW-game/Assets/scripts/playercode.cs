fa1eaf5801476178627045ad155057d7d472b25e
YodaOnCrack <mykyta006@gmail.com>
2024-01-26 18:55:55 +0100

    inventory screen

--- WW-game/Assets/scripts/playercode.cs
+++ WW-game/Assets/scripts/playercode.cs
@@ -1,23 +1,53 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
     public InventoryItem[] inventoryItems;
+    public Canvas menuCanvas;
+    public Text inventoryText;
 
     void Start()
     {
         inventoryItems = new InventoryItem[5];
         inventoryItems[0] = new InventoryItem { itemName = "intropapper", player = gameObject, quantity = 1 };
+
+        // Assuming you have a Canvas in your scene
+        menuCanvas.enabled = false;
     }
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.I))
         {
-            AddItemToInventory("SomeItem", 3);
+            ToggleMenu();
         }
+    }
+
+    void ToggleMenu()
+    {
+        menuCanvas.enabled = !menuCanvas.enabled;
+
+        if (menuCanvas.enabled)
+        {
+            UpdateInventoryText();
+        }
+    }
+
+    void UpdateInventoryText()
+    {
+        // Update the UI Text with inventory information
+        string inventoryInfo = "Inventory:\n";
+        foreach (InventoryItem item in inventoryItems)
+        {
+            if (item != null)
+            {
+                inventoryInfo += item.itemName + " - " + item.quantity + "\n";
+            }
+        }
+        inventoryText.text = inventoryInfo;
     }
 
     public void AddItemToInventory(string itemName, int amount = 1)
@@ -28,6 +58,10 @@ public class Player : MonoBehaviour
         {
             item.quantity += amount;
             Debug.Log("Added " + amount + " " + itemName + "(s) to the inventory.");
+            if (menuCanvas.enabled)
+            {
+                UpdateInventoryText();
+            }
         }
         else
         {
@@ -46,6 +80,4 @@ public class Player : MonoBehaviour
         }
         return null;
     }
-
-
 }
