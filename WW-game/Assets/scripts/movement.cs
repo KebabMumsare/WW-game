09681909a4a420e953a491cf2fe172809a28e9dc
KebabMumsare <jesper.ahlstrom@elev.ga.ntig.se>
2024-01-26 19:53:05 +0100

    gravididet

--- WW-game/Assets/scripts/movement.cs
+++ WW-game/Assets/scripts/movement.cs
@@ -6,7 +6,17 @@ public class movement : MonoBehaviour
 {
     public CharacterController controller;
 
-    public float speed = 12f;
+    public Transform groundCheck;
+
+    public float groundDistance = 0.4f;
+
+    public LayerMask groundMask;
+
+    public float speed = 6f;
+    public float gravity = -9.82f;
+
+    Vector3 velocity;
+    bool isGrounded;
 
     // Start is called before the first frame update
     void Start()
@@ -17,11 +27,39 @@ public class movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
         Vector3 move = transform.right * x + transform.forward * z;
 
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            speed = 12f;
+        }
+        else
+        {
+            speed = 6f;
+        }
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            velocity.y -= gravity * Time.deltaTime;
+        }
+        else
+        {
+            velocity.y += gravity * Time.deltaTime;
+
+            controller.Move(velocity * Time.deltaTime);
+        }
+
         controller.Move(move * speed * Time.deltaTime);
+
+        
     }
 }
